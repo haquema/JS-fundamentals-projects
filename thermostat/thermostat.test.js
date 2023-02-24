112a1f0d8301a1012c31186e90333ed17e920db8
mahaque <haquema@outlook.com>
2023-02-24 00:12:35 +0000

    write tests for up() method: when called once and twice respectively

--- thermostat/thermostat.test.js
+++ thermostat/thermostat.test.js
@@ -8,4 +8,20 @@ describe("Thermostat", () => {
       expect(temp).toBe(20);
     });
   });
+
+  describe("up()", () => {
+    it("increases the temperature by 1", () => {
+      let thermostat = new Thermostat();
+      thermostat.getTemperature();
+      expect(thermostat.getTemperature()).toBe(21)
+    })
+
+    it("when called twice, it increases the temp by 2", () => {
+      let thermostat = new Thermostat();
+      thermostat.getTemperature();
+      thermostat.getTemperature();
+      expect(thermostat.getTemperature()).toBe(22)
+    })
+  })
+
 });
