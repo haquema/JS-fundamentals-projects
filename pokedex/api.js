1348c4c1fb34d3353be71e3d6a09678006afcbfe
mahaque <haquema@outlook.com>
2023-03-09 22:33:07 +0000

    delete files

--- pokedex/api.js
+++
@@ -1,7 +0,0 @@
-const fetchPokemon = (pokemon) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-};
-
-module.exports = fetchPokemon;
